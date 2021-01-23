b198cceb252a918eb521817c4efc9e286f5e8d89
bircan çiftçi <bircan.199798@gmail.com>
2021-01-23 03:09:54 +0300

    Proje dosyası ekle.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> kirtasiyeUrunleri = new Dictionary<int, string>();
+            kirtasiyeUrunleri.Add(1, "Kırmızı Kalem");
+            kirtasiyeUrunleri.Add(2, "Sınav Silgisi");
+            kirtasiyeUrunleri.Add(3, "Tükenmez Kalem");
+            kirtasiyeUrunleri.Add(4, "Kalemlik");
+            kirtasiyeUrunleri.Add(5, "Defter");
+            kirtasiyeUrunleri.Add(6, "Matematik Kitabı");
+            kirtasiyeUrunleri.Add(7, "Notebook");
+            kirtasiyeUrunleri.Add(8, "Fosforlu Kalem");
+
+            foreach (KeyValuePair<int, string> kirtasiye in kirtasiyeUrunleri)
+            {
+                Console.WriteLine(kirtasiye.Key + " " + kirtasiye.Value);
+            }
+        }
+    }
+}
